7da6ffacf4f869b9784f811b1e675a732ab38df3
Rose <rrortegaa@gmail.com>
2025-01-07 11:28:40 -0600

    add styles and config to fullCalendar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,92 @@
-import { Component } from '@angular/core';
+import { Component, signal, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import {
+  CalendarOptions,
+  DateSelectArg,
+  EventClickArg,
+  EventApi,
+} from '@fullcalendar/core'; // useful for typechecking
+import interactionPlugin from '@fullcalendar/interaction'; // for selectable
+import dayGridPlugin from '@fullcalendar/daygrid'; // for dayGridMonth view
+import timeGridPlugin from '@fullcalendar/timegrid'; // for timeGridWeek and timeGridDay views
+import listPlugin from '@fullcalendar/list'; // for list view
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import { h } from '@fullcalendar/core/preact.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FullCalendarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'full-calendar';
+  title = 'fullCalendar';
+  calendarVisible = signal(true);
+  calendarOptions = signal<CalendarOptions>({
+    // plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
+    plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
+    headerToolbar: {
+      left: 'title',
+      right: 'prev,next today',
+    },
+    initialView: 'dayGridMonth',
+    initialEvents: INITIAL_EVENTS,
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
+  });
+  currentEvents = signal<EventApi[]>([]);
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+  handleCalendarToggle() {
+    this.calendarVisible.update((bool) => !bool);
+  }
+
+  handleWeekendsToggle() {
+    this.calendarOptions.update((options) => ({
+      ...options,
+      weekends: !options.weekends,
+    }));
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (
+      confirm(
+        `Are you sure you want to delete the event '${clickInfo.event.title}'`
+      )
+    ) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents.update(() => events);
+    this.changeDetectorRef.detectChanges();
+  }
 }
---
+++ src/app/event-utils.ts
@@ -0,0 +1,37 @@
+import { EventInput } from '@fullcalendar/core';
+
+let eventGuid = 0;
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const TOMORROW_STR = new Date(new Date().setDate(new Date().getDate() + 1))
+  .toISOString()
+  .replace(/T.*$/, ''); // YYYY-MM-DD of tomorrow
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: createEventId(),
+    title: 'test 1',
+    start: TODAY_STR,
+  },
+  {
+    id: createEventId(),
+    title: 'test 2',
+    start: TODAY_STR + 'T00:00:00',
+    end: TODAY_STR + 'T03:00:00',
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: TODAY_STR + 'T12:00:00',
+    end: TODAY_STR + 'T15:00:00',
+  },
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: TODAY_STR,
+    end: TOMORROW_STR,
+  },
+];
+
+export function createEventId() {
+  return String(eventGuid++);
+}
